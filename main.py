9291acb4b73e28dc1939d95b56652dfad748902c
Sofia-1981 <75522555+Sofia-1981@users.noreply.github.com>
2023-04-18 15:54:49 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ import telebot
 import bs4
 from bs4 import BeautifulSoup
 import requests
+import openai
 
-bot = telebot.TeleBot('')
+bot = telebot.TeleBot(' ')
+openai.api_key = ' '
 @bot.message_handler(commands=['start'])
 def start(message):
     mess = f'Привет, {message.from_user.first_name} {message.from_user.last_name}'
@@ -17,10 +19,17 @@ def get_text(message):
         response = requests.get(message.text)
         soup = BeautifulSoup(response.text, features='html.parser')
         text_ = soup.get_text()
-        bot.send_message(message.chat.id, text_, parse_mode='html')
+        response1 = openai.Completion.create(
+        model="text-davinci-003",
+        prompt="Обобщи текст: text_",
+        temperature=0.7,
+        max_tokens=500,
+        top_p=1.0,
+        frequency_penalty=0.0,
+        presence_penalty=1
+        )
+        bot.send_message(message.chat.id, text=response1['choices'][0]['text'])
     else:
         bot.send_message(message.chat.id, message.text, parse_mode='html')
-
-
 
 bot.polling(none_stop=True, interval=0)
