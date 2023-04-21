f1bd4244735bbc6eefaddfaace83072a76912171
Sofia-1981 <75522555+Sofia-1981@users.noreply.github.com>
2023-04-21 11:36:42 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from bs4 import BeautifulSoup
 import requests
 import openai
 
-
 bot = telebot.TeleBot(config.Tg_token)
 openai.api_key = config.OpAi_token
 @bot.message_handler(commands=['start'])
@@ -21,39 +20,46 @@ def get_text(message):
         soup = BeautifulSoup(response.text, features='html.parser')
         text_ = soup.get_text()
         text_spl = text_.split()
-        if len(text_spl) > 2700:
-            sum_res = ''
-            for i in range(0, len(text_spl), 2700):
-                text_i = text_[i:i+2700]
-                response1 = openai.ChatCompletion.create(
-                model="gpt-3.5-turbo",
-                messages=[{"role": "system", "content": "Ты ассистент, который обобщает текст."},
-                        {"role": "user", "content": text_i}],
-                temperature=0.7,
-                max_tokens=1350,
-                top_p=1.0,
-                frequency_penalty=0.0,
-                presence_penalty=1
-                )
-                result = response1['choices'][0]['message']['content']
-                sum_res += result
-            if len(sum_res) < 4096:
-                    bot.send_message(message.chat.id, sum_res)
-            else:
-                    bot.send_message(message.chat.id, result)
+        sum_res = ''
+        for i in range(0, len(text_spl), 700):
+            i = text_spl[i:i+700]
+            i = ' '.join(i)
+            response1 = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[{"role": "system", "content": "Ты ассистент, который обобщает текст."},
+                     {"role": "user", "content": i}],
+            temperature=0.7,
+            max_tokens=1550,
+            top_p=1.0,
+            frequency_penalty=0.0,
+            presence_penalty=1
+            )
+            result = response1['choices'][0]['message']['content']
+            sum_res = sum_res + result
+
+        sum_res1 = ' '
+        response2 = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[{"role": "system", "content": "Ты ассистент, который обобщает текст."},
+                     {"role": "user", "content": sum_res}],
+            temperature=0.7,
+            max_tokens=2000,
+            top_p=1.0,
+            frequency_penalty=0.0,
+            presence_penalty=1
+            )
+        result2 = response2['choices'][0]['message']['content']
+        sum_res1 = sum_res1 + result2
+
+        if len(sum_res1) < 4096:
+            bot.send_message(message.chat.id, sum_res1)
         else:
-                response1 = openai.ChatCompletion.create(
-                model="gpt-3.5-turbo",
-                messages=[{"role": "system", "content": "Ты ассистент, который обобщает текст."},
-                          {"role": "user", "content": text_}],
-                temperature=0.7,
-                max_tokens=2000,
-                top_p=1.0,
-                frequency_penalty=0.0,
-                presence_penalty=1
-                )
-                bot.send_message(message.chat.id, response1['choices'][0]['message']['content'])
+            for j in range(0, len(sum_res1)):
+                j = sum_res1[j:j+4090]
+                bot.send_message(message.chat.id, j)
+
     else:
         bot.send_message(message.chat.id, message.text, parse_mode='html')
 
 bot.polling(none_stop=True, interval=0)
+
