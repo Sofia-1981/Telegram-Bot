bb3cca41eda449ccb68765d9450f06c853fac555
Sofia-1981 <75522555+Sofia-1981@users.noreply.github.com>
2023-04-28 13:20:38 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -6,11 +6,12 @@ from bs4 import BeautifulSoup
 import requests
 import openai
 import time
+from datetime import datetime
 
 bot = telebot.TeleBot(config.Tg_token)
 openai.api_key = config.OpAi_token
-
-
+rate_limit_min = 3
+delay = 69/rate_limit_min
 def chat_gpt_call(request, _temperature, _max_tokens):
     response1 = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
@@ -23,7 +24,6 @@ def chat_gpt_call(request, _temperature, _max_tokens):
         presence_penalty=1
     )
     return response1['choices'][0]['message']['content']
-
 
 @bot.message_handler(commands=['start'])
 def start(message):
@@ -42,24 +42,23 @@ def get_text(message):
         while a:
             a = False
             sum_res = ''
-            try:
-                for i in range(0, len(text_spl_clear), 2000):
-                    text_chunk_array = text_spl_clear[i:i+2000]
-                    text_chunk = ' '.join(text_chunk_array)
-                    result = chat_gpt_call(text_chunk, 0.7, 1000)
-                    sum_res = sum_res + " " + result
-                result_sum = chat_gpt_call(sum_res, 0.7, 1000)
-            except openai.error.RateLimitError:
-                    time.sleep(20)
-                    result = chat_gpt_call(text_chunk, 0.7, 1000)
-                    sum_res = sum_res + " " + result
-                    time.sleep(5)
-                    result_sum = chat_gpt_call(sum_res, 0.7, 1000)
+            for i in range(0, len(text_spl_clear), 2000):
+                text_chunk_array = text_spl_clear[i:i + 2000]
+                text_chunk = ' '.join(text_chunk_array)
+                time_request = []
+                result = chat_gpt_call(text_chunk, 0.7, 1000)
+                t = datetime.now()
+                time_request.append(t)
+                sum_res = sum_res + " " + result
+                t1 = datetime.now()
+                dif1 = t1 - time_request[0]
+                if dif1.seconds < delay:
+                    time.sleep(delay - dif1.seconds)
+            result_sum = chat_gpt_call(sum_res, 0.7, 1000)
             if len(result_sum) >= 2000:
-                text_spl_clear = result_sum
+                text_spl = list(result_sum)
             else:
                 bot.send_message(message.chat.id, result_sum)
-                text_spl_clear = result_sum
     else:
         bot.send_message(message.chat.id, message.text, parse_mode='html')
 
