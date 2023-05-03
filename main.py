484481868eb522c69e36ba86ed0ebd80c06a09dc
Sofia-1981 <75522555+Sofia-1981@users.noreply.github.com>
2023-05-03 09:20:47 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import logging
 import re
 import telebot
 import config
@@ -7,7 +8,7 @@ import requests
 import openai
 import time
 from datetime import datetime
-
+from telebot import types
 bot = telebot.TeleBot(config.Tg_token)
 openai.api_key = config.OpAi_token
 rate_limit_min = 3
@@ -25,6 +26,7 @@ def chat_gpt_call(request, _temperature, _max_tokens):
     )
     return response1['choices'][0]['message']['content']
 
+
 @bot.message_handler(commands=['start'])
 def start(message):
     mess = f'Hello, {message.from_user.first_name} {message.from_user.last_name}'
@@ -38,27 +40,42 @@ def get_text(message):
         text_ = soup.get_text().replace('\n', ' ')
         text_spl = text_.split(" ")
         text_spl_clear = list(filter(None, text_spl))
-        a = True
-        while a:
-            a = False
-            sum_res = ''
-            for i in range(0, len(text_spl_clear), 2000):
-                text_chunk_array = text_spl_clear[i:i + 2000]
-                text_chunk = ' '.join(text_chunk_array)
-                time_request = []
-                result = chat_gpt_call(text_chunk, 0.7, 1000)
-                t = datetime.now()
-                time_request.append(t)
-                sum_res = sum_res + " " + result
-                t1 = datetime.now()
-                dif1 = t1 - time_request[0]
-                if dif1.seconds < delay:
-                    time.sleep(delay - dif1.seconds)
-            result_sum = chat_gpt_call(sum_res, 0.7, 1000)
-            if len(result_sum) >= 2000:
-                text_spl = list(result_sum)
-            else:
-                bot.send_message(message.chat.id, result_sum)
+        try:
+            a = True
+            while a:
+                a = False
+                sum_res = ''
+                for i in range(0, len(text_spl_clear), 2000):
+                    text_chunk_array = text_spl_clear[i:i + 2000]
+                    text_chunk = ' '.join(text_chunk_array)
+                    time_request = []
+                    result = chat_gpt_call(text_chunk, 0.7, 1000)
+                    t = datetime.now()
+                    time_request.append(t)
+                    sum_res = sum_res + " " + result
+                    t1 = datetime.now()
+                    dif1 = t1 - time_request[0]
+                    if dif1.seconds < delay:
+                        time.sleep(delay - dif1.seconds)
+                result_sum = chat_gpt_call(sum_res, 0.7, 1000)
+                if len(result_sum) >= 2000:
+                    text_spl_clear = list(result_sum)
+                else:
+                    bot.send_message(message.chat.id, result_sum)
+        except:
+                markup = types.InlineKeyboardMarkup()
+                butt = types.InlineKeyboardButton('Try again in 20 sec', callback_data='send')
+                markup.add(butt)
+                bot.send_message(message.chat.id, 'Unsuccessful attempt', reply_markup=markup)
+
+                @bot.callback_query_handler(func=lambda callback: True)
+                def callback_message(callback):
+                    if callback.data == 'send':
+                        bot.edit_message_text(chat_id=callback.message.chat.id, message_id=callback.message.message_id,
+                                              text='Expect...')
+                        get_text(message)
+
+                pass
     else:
         bot.send_message(message.chat.id, message.text, parse_mode='html')
 
